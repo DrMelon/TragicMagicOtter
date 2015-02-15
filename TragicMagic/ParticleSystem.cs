2b177d6edc1359ec881e2a57b47b70215fbef966
J Brown <drmelon.mail@gmail.com>
2015-02-15 20:33:33 +0000

    Working on particlesystem, comitting to transfer to laptop

---
+++ TragicMagic/ParticleSystem.cs
@@ -0,0 +1,30 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TragicMagic;
+
+//@Author: J. Brown (DrMelon)
+//@Date: 15/02/15
+//@Purpose: This is a particle system for use in spells, etc. These can be loaded from and saved to files. 
+//@Usage: To use in-game, instantiate a particlesystem object and set the parameters manually or load from file. Add to the scene, and call Start or Stop to start/stop emitting.
+
+namespace TragicMagic
+{
+    class ParticleSystem : Entity
+    {
+        // List of particles still in-scene.
+        List<Particle> livingParticles;
+
+
+
+        public ParticleSystem()
+        {
+
+        }
+
+
+    }
+}
